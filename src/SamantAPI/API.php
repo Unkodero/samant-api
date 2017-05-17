a53dc7f344f510ed7a117dc42e3b08acc64b079b
Unkodero <Unkodero@users.noreply.github.com>
2017-05-17 22:13:01 +0300

    Update API.php

--- src/SamantAPI/API.php
+++ src/SamantAPI/API.php
@@ -2,7 +2,6 @@
 
 namespace SamantAPI;
 
-
 use SamantAPI\Exception\ApiException;
 use SamantAPI\Exception\AuthException;
 use SamantAPI\Exception\RequestException;
@@ -16,42 +15,28 @@ class API
     /**
      * API URL
      */
-    const API_URL = 'http://samant.deacrm.ru/rest/orders/';
+    const API_URL = 'http://samant.deacrm.ru/rest/';
 
     /**
-     * AUTH URL
+     * @var
      */
-    const AUTH_URL = 'http://samant.deacrm.ru/login.html';
-
+    private $username;
     /**
-     * @var null|string
+     * @var
      */
-    private $cookie = null;
+    private $token;
 
     /**
      * API constructor.
      *
      * @param $username
-     * @param $password
+     * @param $token
      * @throws AuthException
      */
-    public function __construct($username, $password)
+    public function __construct($username, $token)
     {
-        $this->cookie = dirname(__FILE__) . '/Storage/cookie.txt';
-
-        $response = $this->request(self::AUTH_URL, [
-            CURLOPT_POST => true,
-            CURLOPT_POSTFIELDS => http_build_query([
-                'username' => $username,
-                'password' => $password,
-                'returnUrl' => 'login.html',
-                'service' => 'login'
-            ])
-        ], true);
-
-        if (!preg_match("/Выход/", $response)) {
-            throw new AuthException('401 Unauthorized');
-        }
+        $this->username = $username;
+        $this->token = $token;
     }
 
     /**
@@ -59,9 +44,9 @@ class API
      * @param $id - Order id
      * @return array
      */
-    public function getOrder($id)
+    public function getOrder($id = null)
     {
-        return $this->request(self::API_URL . $id);
+        return $this->request("Orders/{$id}");
     }
 
     /**
@@ -81,10 +66,12 @@ class API
      */
     public function sendOrder(Model\OrderModel $order)
     {
-        $order = $this->request(self::API_URL, [
+        $order = $this->request('Orders', [
             CURLOPT_POST => true,
             CURLOPT_POSTFIELDS => http_build_query([
-                'Customer' => get_object_vars($order)
+                'Customer' => get_object_vars($order),
+                'username' => $this->username,
+                'token' => $this->token
             ]),
             CURLOPT_HTTPHEADER => ["Content-type: multipart/form-data"]
         ]);
@@ -97,43 +84,36 @@ class API
     }
 
     /**
-     * @param $url
+     * @param $method
      * @param array $parameters
-     * @param $isAuth
-     *
      * @return mixed
-     *
-     * @throws RequestException
-     * @throws AuthException
      * @throws ApiException
+     * @throws RequestException
      */
-    private function request($url, $parameters = [], $isAuth = false)
+    private function request($method, $parameters = [])
     {
+        $url = self::API_URL . "{$method}/?username={$this->username}&token={$this->token}";
+
         $curl = curl_init($url);
 
         curl_setopt_array($curl, $parameters + [
                 CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_COOKIEJAR => $this->cookie,
-                CURLOPT_COOKIEFILE => $this->cookie
-            ]);
+                ]);
 
         $response = curl_exec($curl);
-
-        if (!$isAuth) {
-            $response = json_decode($response, true);
-        }
+        curl_close($curl);
 
         if ($response === false) {
             throw new RequestException(curl_error($curl));
-        } elseif (curl_getinfo($curl, CURLINFO_RESPONSE_CODE) === 401) {
-            throw new AuthException('401 Unauthorized');
-        } elseif (!$isAuth && !$response['success']) {
+        } else {
+            $response = json_decode($response, true);
+        }
+
+        if (!$response['success']) {
             throw new ApiException($response['message']);
         }
 
-        curl_close($curl);
-
-        return ($isAuth ? $response : $response['data']);
+        return $response['data'];
     }
 
 }
